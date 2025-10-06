29a5ae457ac3cda64c7c790f8a4b0b014f6bde83
mauris <mauris.salazar105@gmail.com>
2025-10-05 21:48:00 -0400

    Hola estoy subiendo el primer ejercicio hecho con el lenguaje python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/maurinstam.py
@@ -0,0 +1,57 @@
+'''
+Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+'''
+
+#Hola estoy usando python
+
+#https://www.python.org/ 
+
+'''
+
+Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+'''
+
+# comentario de una linea 
+
+'''
+comentario de varias lineas
+
+'''
+
+'''
+
+Crea una variable (y una constante si el lenguaje lo soporta).
+
+'''
+miVariable = "variables"
+
+miVariable = "otraVariable"
+
+
+'''
+
+Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...
+
+'''
+
+entero = 1
+real = 3.16
+caracter = "Mi nombre es Mauris"
+booleano1 = True
+booleano2 = False
+
+
+'''
+Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje
+
+'''
+
+print ("Hola, estoy usando el lenguaje de programacion python")
+
+print(type(entero))
+print(type(real))
+print(type(caracter))
+print(type(booleano1))
+print(type(booleano2))
